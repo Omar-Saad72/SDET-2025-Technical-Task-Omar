0c1745276b99535365910daed462d23fb95af2c4
Omar Saad <omarsaad124@yahoo.com>
2025-08-12 20:14:27 +0300

    Adding Nightwatch Config Files

---
+++ task2-linkedin/nightwatch.conf.js
@@ -0,0 +1,63 @@
+module.exports = {
+
+  src_folders: ['tests'],
+
+  page_objects_path: ['page_objects'],
+
+  plugins: [],
+  
+  globals_path: '',
+  
+  webdriver: {},
+
+  test_workers: {
+    enabled: true
+  },
+
+  test_settings: {
+    default: {
+      disable_error_log: false,
+      launch_url: 'http://localhost',
+
+      screenshots: {
+        enabled: false,
+        path: 'screens',
+        on_failure: true
+      },
+
+      desiredCapabilities: {
+        browserName: 'chrome'
+      },
+      
+      webdriver: {
+        start_process: true,
+        server_path: ''
+      },
+      
+    },
+    
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          w3c: true,
+          args: [
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            '--headless'
+          ]
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
+    
+  },
+};
---
+++ task3-my-store/nightwatch.conf.js
@@ -0,0 +1,56 @@
+module.exports = {
+
+  src_folders: ['tests'],
+
+  page_objects_path: ['page_objects'],
+
+  test_workers: {
+    enabled: true
+  },
+
+  test_settings: {
+    default: {
+      disable_error_log: false,
+      launch_url: 'http://localhost',
+
+      screenshots: {
+        enabled: false,
+        path: 'screens',
+        on_failure: true
+      },
+
+      desiredCapabilities: {
+        browserName: 'chrome'
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: ''
+      },
+
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          w3c: true,
+          args: [
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            '--headless'
+          ]
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
+  },
+};
