4300437f038809cd00f41caefa0842b6ff899830
Omar Saad <52426308+Omar-Saad72@users.noreply.github.com>
2025-08-12 21:27:58 +0300

    Update nightwatch.conf.js

--- task1-achme-chemicals/nightwatch.conf.js
+++ task1-achme-chemicals/nightwatch.conf.js
@@ -77,10 +77,9 @@ module.exports = {
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: true,
           args: [
-            //'--no-sandbox',
-            //'--ignore-certificate-errors',
-            //'--allow-insecure-localhost',
-            //'--headless'
+            '--no-sandbox',
+            "--disable-gpu",
+            '--headless'
           ]
         }
       },
